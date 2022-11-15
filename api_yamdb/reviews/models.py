b74fbdaabb39bb0904121336a6196e32d4660fa1
Denis <almazbr@gmail.com>
2022-11-15 18:54:27 +0300

    add model Title and validator

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from .validators import year_validator
+
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='название',
+        max_length=200,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='жанр',
+        related_name='titles',
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        max_length=400,
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='категория',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    year = models.IntegerField(
+        verbose_name='год',
+        validators=(year_validator,),
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+
+    def __str__(self):
+        return self.name
