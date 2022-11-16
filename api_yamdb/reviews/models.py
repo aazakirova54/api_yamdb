8eb2e3ec5e4275bdcb163e8e5ef1a2f0db884413
Оля <olyakorcha@gmail.com>
2022-11-16 13:31:33 +0300

    add model Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,50 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+TEXT_LIMIT = 15
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+    )
+    score = models.IntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MaxValueValidator(10, 'Максимальное значение — 10'),
+            MinValueValidator(1, 'Минимальное значение — 1')
+        ],
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        default_related_name = 'reviews'
+        ordering = ['-pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_name_review'
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:TEXT_LIMIT]
+
+
+class Comment(models.Model):
+    pass
