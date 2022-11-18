1365fa64b45198af35889e25858cb2ff69721069
Denis <almazbr@gmail.com>
2022-11-18 20:20:36 +0300

    fix error review 2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -64,14 +64,13 @@ class ReviewSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, data):
-        title_id = self.context.get('view').kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
         request = self.context['request']
         author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
         if request.method == 'POST':
-            if Review.objects.filter(author, title).exists():
-                raise serializers.ValidationError(
-                    'Можно оставить только один отзыв на произведение')
+            if Review.objects.filter(title=title, author=author).exists():
+                raise serializers.ValidationError('Отзыв уже существует!')
         return data
 
     def validate_score(self, data):
