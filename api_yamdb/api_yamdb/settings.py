f2df306fbb07fdae9a841c36483141485edb7706
Оля <olyakorcha@gmail.com>
2022-11-18 15:55:16 +0300

    fix settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,10 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'reviews.apps.ReviewsConfig',
-    'api.apps.ApiConfig',
     'api',
-    'reviews'
+    'reviews',
 ]
 
 MIDDLEWARE = [
