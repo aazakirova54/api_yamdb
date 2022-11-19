4de4c09740785e3b61d4006eda35c4c784286bb3
Denis <45938921+lepag-jun@users.noreply.github.com>
2022-11-19 14:24:34 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -77,17 +77,15 @@ class CommentViewSet(ModelViewSet):
     permission_classes = (IsAuthorOrAdminOrModerator,)
 
     def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
-        return Comment.objects.filter(review=review)
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        return review.comments.all()
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, pk=review_id)
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
 
 
